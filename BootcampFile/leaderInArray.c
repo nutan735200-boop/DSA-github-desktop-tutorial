9159d541191c7f92698a20981217be66ca47330a
nutan735200-boop <nutan735200@gmail.com>
2026-05-27 23:12:47 +0530

    Create leaderInArray.c
    Implemented leaders in array program in C

---
+++ BootcampFile/leaderInArray.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements of the array:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int maxRight=arr[n-1];
+    printf("Leaders in array are: %d", maxRight);
+    for(int i=n-2;i>=0;i--){
+        if(arr[i]>maxRight){
+            maxRight=arr[i];
+            printf("%d ", maxRight);
+        }
+    }
+    return 0;
+}
