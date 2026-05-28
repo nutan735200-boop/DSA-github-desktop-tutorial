427861de02d6cf49ac1261966daf8e0929a541bf
nutan735200-boop <nutan735200@gmail.com>
2026-05-28 15:22:38 +0530

    Create ConsonantCount.c
    Implemented Consonant count in string in  C

---
+++ BootcampFile/ConsonantCount.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    int count=0;
+    printf("Enter a string: ");
+    scanf("%[^\n]s", str);
+    for(int i=0;str[i]!='\0';i++){
+        if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z')){
+            if(!(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')){
+                count++;
+            }
+        }
+    }
+    printf("Number of consonants in the string: %d", count);
+    return 0;
+}
