030f747d62f5d1f024bfd5363b18e5a88b85c439
nutan735200-boop <nutan735200@gmail.com>
2026-05-31 23:49:59 +0530

    Create LongestCommonSubsequence.java
    Implemented java program to find longest common subsequence

---
+++ BootcampFile/LongestCommonSubsequence.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class LongestCommonSubsequence{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 1st String: ");
+        String str1=sc.nextLine();
+        System.out.println("Enter 2nd String: ");
+        String str2=sc.nextLine();
+        int n=str1.length();
+        int m=str2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+               if(i==0||j==0){
+                dp[i][j]=0;
+               }
+               else if(str1.charAt(i-1)==str2.charAt(j-1)){
+                dp[i][j]=1+dp[i-1][j-1];
+               }
+               else{
+                dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+               }
+            }
+        }
+        System.out.println("Longest Common Subsequence: "+dp[n][m]);
+        StringBuilder sb=new StringBuilder();
+        int i=n,j=m;
+        while(i>0&&j>0){
+            if(str1.charAt(i-1)==str2.charAt(j-1)){
+                sb.append(str1.charAt(i-1));
+                i--;
+                j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+        System.out.println("Longest Common Subsequence String: " + sb.reverse().toString());
+    }
+}
