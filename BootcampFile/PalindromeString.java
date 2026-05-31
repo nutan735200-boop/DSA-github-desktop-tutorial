a63f3e2ea54afa85eca1fa6ec31e1650e9be19a6
nutan735200-boop <nutan735200@gmail.com>
2026-05-31 14:02:10 +0530

    Create PalindromeString.java
    Implemented java program to Check palindrome string

---
+++ BootcampFile/PalindromeString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class PalindromeString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str=sc.nextLine();
+        int l=0;
+        int r=str.length()-1;
+        boolean isPalindrome=true;
+        while(l<r){
+            if(str.charAt(l)!=str.charAt(r)){
+                isPalindrome=false;
+                break;
+            }
+            l++;
+            r--;
+        }
+        if(isPalindrome){
+            System.out.println(str+" is a palindrome");
+        }
+        else{
+            System.out.println(str+" is not a palindrome");
+        }
+
+    }
+}
