13d37377235c07abeb007e5e2fb65dcf271857f7
nutan735200-boop <nutan735200@gmail.com>
2026-05-31 14:02:44 +0530

    Create RemoveDuplicateCharacter.java
    Implemented java program to remove duplicate character

---
+++ BootcampFile/RemoveDuplicateCharacter.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class RemoveDuplicateCharacter{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str=sc.nextLine();
+        String result="";
+        for(int i=0;i<str.length();i++){
+            char c=str.charAt(i);
+            if(result.indexOf(c)==-1){
+                result+=c;
+            }
+        }
+        System.out.println("String after removing duplicates: "+result);
+    }
+}
